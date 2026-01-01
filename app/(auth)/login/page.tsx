26ff49a52bb9fdb2c2bb837a52b784957823a097
Shreejan Regmi <shreejan.regmi@gmail.com>
2026-01-01 22:32:35 +0545

    login form ui

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,66 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export default function LoginPage({ className }: React.ComponentProps<"div">) {
+  return (
+    <div className="w-screen h-screen grid place-items-center">
+      <div className={cn("flex flex-col gap-6 w-[500px]", className)}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </Field>
+                <Field>
+                  <div className="flex items-center">
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    {/* <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a> */}
+                  </div>
+                  <Input id="password" type="password" required />
+                </Field>
+                <Field>
+                  <Button type="submit">Login</Button>
+                  <FieldDescription className="text-center">
+                    Don&apos;t have an account? <a href="#">Sign up</a>
+                  </FieldDescription>
+                </Field>
+              </FieldGroup>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar";
+
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <NavBar />
+      {children}
+    </>
+  );
+}
