73f369f05a1d34b272d3c1920df895c3f7137897
Shreejan Regmi <shreejan.regmi@gmail.com>
2026-01-05 23:14:17 +0545

    show user data in profile dropdown

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -9,14 +9,16 @@ import {
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ChevronDown, LogOut, ShoppingBag, User } from "lucide-react";
+import { getOwnUserData } from "@/services/user.service";
 
 export async function UserNav() {
+  const user = await getOwnUserData();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="rounded-full">
           <User className="h-4 w-4" />
-          <p>Shreejan</p>
+          <p>{user.firstName}</p>
           <ChevronDown className="mt-1" />
         </Button>
       </DropdownMenuTrigger>
@@ -24,8 +26,8 @@ export async function UserNav() {
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">My Account</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              user@example.com
+            <p className="text-xs leading-none text-muted-foreground mt-1">
+              {user.email}
             </p>
           </div>
         </DropdownMenuLabel>
@@ -48,10 +50,7 @@ export async function UserNav() {
         <DropdownMenuSeparator />
 
         {/* The Logout Button triggers the Server Action */}
-        <DropdownMenuItem
-          className="cursor-pointer text-red-600 focus:text-red-600"
-          // onClick={() => logout()}
-        >
+        <DropdownMenuItem className="cursor-pointer text-red-600 focus:text-red-600">
           <a href={"/logout"} className="flex items-center gap-4 w-full">
             <LogOut className="h-4 w-4" />
             <p>Log out</p>
---
+++ services/user.service.ts
@@ -0,0 +1,20 @@
+import { fetcher } from "@/lib/utils";
+import { User } from "@/types/user";
+import { cookies } from "next/headers";
+export async function getOwnUserData(): Promise<User> {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  if (!accessToken) {
+    throw new Error("No access token found");
+  }
+  return fetcher(`/user/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: accessToken,
+    },
+    // next: {
+    //   tags: ["loggedInUser"],
+    // },
+  });
+}
